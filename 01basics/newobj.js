9b131c77c5a47b37037466168a4fc961314ef698
sakshamsingh6106 <saksham6106@gmail.com>
2026-06-19 12:40:23 +0000

    swith

--- 01basics/newobj.js
+++ 01basics/newobj.js
@@ -1,4 +1,23 @@
-console.log(b);
-var b=89;
-let c;
-console.log(c);
+let month=3;
+switch (month) {
+    case 1:
+         console.log("key matched");
+         console.log("month is jan"); 
+        break;
+     case 2:
+         console.log("key matched");
+         console.log("month is feb"); 
+        break;
+     case 3:
+         console.log("key matched");
+         console.log("month is mar"); 
+        // break;       
+     case 4:
+         console.log("key matched");
+         console.log("month is april"); 
+        break;
+    default:
+        console.log("no key matched");
+        console.log("value out of the range of 1-12");
+        break;
+}
