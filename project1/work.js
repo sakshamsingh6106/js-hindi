7e806175db3747ab18ccc4972d1f15c0557be4f5
sakshamsingh6106 <saksham6106@gmail.com>
2026-06-30 12:26:38 +0000

    just built my first proper project

---
+++ project1/work.js
@@ -0,0 +1,52 @@
+let value=10;
+let randomNumber =Math.floor(Math.random() * 100 + 1);
+const userInput = document.querySelector('#guessField');
+const submit = document.querySelector('#subt');
+const remaining = document.querySelector('.lastResult');
+const guessSlot = document.querySelector('.guesses');
+const lowOrHi = document.querySelector('.lowOrHi');
+const reset = document.getElementById("reset");
+submit.addEventListener('click', function (e) {
+    e.preventDefault();
+    const guess = parseInt(userInput.value); 
+    if(value<1){
+       lowOrHi.innerHTML=`game over the guess value was:  ${randomNumber}: press reset to play again`
+       reset.style.display = "inline";
+       submit.disabled=true;
+    }
+    else{
+    if(guess===''|| guess<1 || guess>100){
+        alert('PLease enter a valid number');
+    }
+    else{
+        value--;
+        if(guess===randomNumber){
+            lowOrHi.innerHTML="YOU GUESSED IT RIGHT:  press reset button to play again";
+             reset.style.display = "inline";
+             submit.disabled=true;
+        }
+        else{
+          remaining.innerHTML=`${value}`;
+          guessSlot.innerHTML += `${guess}, `;
+          if(guess>randomNumber){
+             lowOrHi.innerHTML="value is high"
+          }
+          else if(guess<randomNumber){
+             lowOrHi.innerHTML="value is low"
+          }
+          userInput.value = '';
+
+
+        }
+       
+    }
+}
+})
+reset.addEventListener("click", function() {
+    value = 10;
+    remaining.innerHTML=`${value}`;
+    guessSlot.innerHTML=""
+    submit.disabled = false;
+    reset.style.display = "none";
+    randomNumber = Math.floor(Math.random() * 100 + 1);
+});
