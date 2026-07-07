4ca3a58e0e52b9e88902106e3e0c3194795e53e9
sakshamsingh6106 <saksham6106@gmail.com>
2026-07-07 14:53:18 +0000

    learned promise

---
+++ promise/promise.js
@@ -0,0 +1,21 @@
+const prom=new Promise(function(resolve,reject){
+    setTimeout(() => {
+        let error=true;
+        if(error==false){
+            resolve({name: "saksham", passwoed: "12345"})
+        }
+        else{
+            reject("yo your program buggin and u thuggin")
+        }
+    }, 1000)
+})
+let ret=prom
+.then(function(user){
+    return user.name;
+})
+.then(function(retarded){
+    console.log(retarded);
+})
+.catch(function(err){
+    console.log(err);
+})
