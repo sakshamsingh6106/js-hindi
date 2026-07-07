a5a7a8a8f773880c9e78e6ab056066cf2dda618e
sakshamsingh6106 <saksham6106@gmail.com>
2026-07-07 16:13:48 +0000

    started learning fetch

--- promise/promise.js
+++ promise/promise.js
@@ -1,21 +1,30 @@
-const prom=new Promise(function(resolve,reject){
-    setTimeout(() => {
-        let error=true;
-        if(error==false){
-            resolve({name: "saksham", passwoed: "12345"})
-        }
-        else{
-            reject("yo your program buggin and u thuggin")
-        }
-    }, 1000)
+// const prom=new Promise(function(resolve,reject){
+//     setTimeout(() => {
+//         let error=true;
+//         if(error==false){
+//             resolve({name: "saksham", passwoed: "12345"})
+//         }
+//         else{
+//             reject("yo your program buggin and u thuggin")
+//         }
+//     }, 1000)
+// })
+// let ret=prom
+// .then(function(user){
+//     return user.name;
+// })
+// .then(function(retarded){
+//     console.log(retarded);
+// })
+// .catch(function(err){
+//     console.log(err);
+// })
+fetch('https://api.github.com/users/hiteshchoudhary')
+.then((response) => {
+    response=response.json();
+    return response
 })
-let ret=prom
-.then(function(user){
-    return user.name;
+.then((data) => {
+    console.log(data);
 })
-.then(function(retarded){
-    console.log(retarded);
-})
-.catch(function(err){
-    console.log(err);
-})
+.catch((error) => console.log(error))
